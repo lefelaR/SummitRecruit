15aef2f17b7df41c61710c3838b72fa64398f9db
rakheoana <rakgew@gmail.com>
2020-03-14 17:05:42 +0200

    navigation

---
+++ summitmovies/src/Nav.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Nav = ()=>{
+    return (
+    <nav>
+
+        <div class="nav-wrapper ciontainer">
+          <a href="#" class="brand-logo">Movies App</a>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+            <li><a href="#">login</a></li>
+          </ul>
+        </div>
+      </nav>
+
+    );
+}
+export default Nav;
